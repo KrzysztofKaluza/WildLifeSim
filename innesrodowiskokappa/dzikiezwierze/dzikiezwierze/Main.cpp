fc581c7382c08c43a8edc4ccd8c4bfa16d2936be
KrzysztofKaluza <krzysztof.kaluza.96@gmail.com>
2021-01-05 19:32:45 +0100

    czyszczenie zbednych plikow

--- innesrodowiskokappa/dzikiezwierze/dzikiezwierze/Main.cpp
+++
@@ -1,7 +0,0 @@
-#include "SimLogic.h"
-
-int main() {
-	SimLogic symulacja;
-	symulacja.start_program();
-	return 0;
-}
--- innesrodowiskokappa/dzikiezwierze/dzikiezwierze/Pole.cpp
+++
@@ -1,6 +0,0 @@
-#include "Pole.h"
-
-string Pole::getRodzaj_pola()
-{
-	return this->rodzaj_pola;
-}
